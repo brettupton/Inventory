e4af294ac0b99331be35889671fb554b3538410a
Brett Upton <brettjupton@gmail.com>
2022-03-21 21:25:43 -0400

    3/21 work

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-import {inventory, MPN} from "./JS/inventory.js";
+import inventoryObjectContainer from "./JS/inventory.js";
 
 const appleID = 2970325;
 const createButton = document.getElementById('create');
 const viewButton = document.getElementById('view');
-const listButton = document.getElementById('showList');
-const objArray = [];
+const addQuantityButton = document.getElementById('addQuantity');
+const updateArrayButton = document.getElementById('updateArray');
+let fullInventory = inventoryObjectContainer;
 
 if (createButton && viewButton) {
     createButton.addEventListener('click', () => {window.location.href="create.html";});
     viewButton.addEventListener('click', () => {window.location.href="view.html";});
 }
-if (listButton) {
-    listButton.addEventListener('click', () => showArrayNames(inventory));
+if (window.location.pathname === "/view.html") {
+    addQuantityButton.addEventListener('click', addToItem);
+    showArrayNames(fullInventory);
+    updateArrayButton.addEventListener('click', (fullInventory) => showArrayNames);
 }
 
-// Create the class that will hold the information for the items in inventory
+// Create the class that will hold the information for the report
 class inventoryItem {
     constructor(name) {
         this.name = name;
@@ -29,36 +32,48 @@ class inventoryItem {
     }
 }
 
-// Create an object that holds the new inventory array and the date created
-function inventoryObj(theArray, theDate) {
-    this.invArray = theArray,
-    this.dateTime = theDate
-}
-
-// Create a new blank inventory and store it in the inventory object
-// Stores the new inventory object into an array
-function createEmptyInventory() {
-    let newArray = [];
-    for (let i = 0; i < inventory.length; i++) {
-        let newItem = new inventoryItem(inventory[i]);
-        newItem.MPN = MPN[i];
-        newArray.push(newItem);
+// Create HTML table elements for each item in the inventory object
+function showArrayNames(obj) {
+    let counter = 0;
+    let tableBody = document.getElementById('tableBody');
+    let tableRow = document.getElementsByClassName("tr");
+    console.log(tableRow.className);
+    let tableData = document.getElementsByClassName("td");
+    tableBody.removeChild(tableRow);
+    tableBody.removeChild(tableData);
+    for (let item in obj) {
+        counter++;
+        let addRow = document.createElement("tr");
+        tableBody.appendChild(addRow);
+        let addNum = document.createElement("td");
+        addNum.id = 'itemNum';
+        let addName = document.createElement("td");
+        addName.id = 'itemName';
+        let addPartNumber = document.createElement("td");
+        addPartNumber.id = 'partNumber';
+        let addQuantity = document.createElement("td");
+        addQuantity.id = `addQuantity${counter}`;
+        addRow.appendChild(addNum);
+        addRow.appendChild(addName);
+        addRow.appendChild(addPartNumber);
+        addRow.appendChild(addQuantity);
+        addNum.innerHTML = counter;
+        addName.innerHTML = obj[item].itemName;
+        addPartNumber.innerHTML = obj[item].partNumber;
+        addQuantity.innerHTML = obj[item].quantity;
     }
-    console.log('inventory created');
-    let invobj = new inventoryObj(newArray, new Date().toString());
-    objArray.push(invobj);
-    return objArray[0];
 }
 
-// Create HTML list elements for each item name in the inventory array
-function showArrayNames(arr) {
-    for (let item of arr) {
-        let addTo = document.createElement("li");
-        let ulList = document.getElementById('viewList');
-        addTo.className = 'list-group-item';
-        ulList.appendChild(addTo);
-        addTo.innerHTML = item;
+function addToItem() {
+    const newQuantity = document.getElementById('newQuantity');
+    const inventoryNum = document.getElementById('itemNum');
+    fullInventory[inventoryNum.value].quantity = newQuantity.value;
+}
+
+function updateArray() {
+    for (let i = 1; i < Object.keys(fullInventory).length; i++) {
+        let quantityElement = document.getElementById(`addQuantity${i}`);
+        //quantityElement.innerHTML = fullInventory[i].quantity;
     }
+    console.log(`addQuantity${4}`);
 }
-
-export {inventoryItem, createEmptyInventory};
