715615e6e8da2dfb88bbbe63161c7bd9ca914ead
brettupton <93960408+brettupton@users.noreply.github.com>
2022-03-28 10:31:44 -0400

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
     try {
-        await mongoose.connect('mongodb+srv://brettjupton:Zelda10!64@cluster0.if89k.mongodb.net/Inventory?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://username:password1@cluster0.if89k.mongodb.net/Inventory?retryWrites=true&w=majority', {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
